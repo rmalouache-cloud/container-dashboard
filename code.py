59a096bada6a8f52a7496e7e3b9d483039d568d5
rmalouache-cloud <rmalouache@gmail.com>
2026-04-06 13:39:48 +0100

    code.py

--- code.py
+++ code.py
@@ -177,7 +177,7 @@ if summary is not None:
     logo_path = "entete.PNG"
 
     if os.path.exists(logo_path):
-        pdf.image(logo_path, x=10, y=8, w=60)  # taille + position
+        pdf.image(logo_path, x=500, y=800, w=60)  # taille + position
 
     # =========================
     # TITLE
